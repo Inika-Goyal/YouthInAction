d9bb986eb0e94819eff13a1fc178a126f3d553ea
Yana <yananin2002@gmail.com>
2025-03-29 13:32:04 -0500

    added login page!

--- youth_in_action/src/App.js
+++ youth_in_action/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+
+import "./App.css";
+import lightsandcubes from "./assets/lights-and-cubes.png";
+import loginicons from "./assets/login-icons.png"; // Ensure you have this image in your assets folder
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="landing-page">
+    <div className="overlap-wrapper">
+      <div className="overlap">
 
+        <div className="overlap-group">
+          <div className="login"  />
+          <div className="login-container">
+            <input 
+              className="input" 
+              id="email"
+              placeholder="Enter Email" 
+            />
+            
+              <input 
+              type="password" 
+              className="input"
+              id="password" 
+              placeholder="Enter Password" 
+            />
+          <div className="illuminate-change">
+            Illuminate Change
+            <br />
+            <br />
+            <br />
+            Empower Communities
+          </div>
+
+          <p className="text-wrapper">
+            Bridging the gap between young changemakers and organizations in
+            need. From environmental action to education and community
+            outreach, we make it easy for teens to give back.
+          </p>
+          <br/>
+          <br/>
+          <br/>
+
+            <button className="login-button">Login</button>
+          </div>
+          <div className="text-wrapper-2">Youth In Action</div>
+          <img
+            className="photo"
+            alt="Photo"
+            src={lightsandcubes}
+          />
+          <img
+            className="photo2"
+            alt="Photo"
+            src={loginicons}
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+);};
 export default App;
