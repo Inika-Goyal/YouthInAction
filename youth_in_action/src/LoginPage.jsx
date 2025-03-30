3044e788ce7d0adc19eac4ed44833723a0c5d7a7
Inika <igoya@uic.edu>
2025-03-29 20:59:09 -0500

    better login page

--- youth_in_action/src/LoginPage.jsx
+++ youth_in_action/src/LoginPage.jsx
@@ -1,12 +1,18 @@
-
 import lightsandcubes from "./assets/lights-and-cubes.png";
 import loginicons from "./assets/login-icons.png"; 
 import "./LoginPage.css";
 import { useNavigate } from "react-router-dom"; 
 
+export const Box = () => {
+  return (
+    <div className="box">
+      <div className="rectangle" />
+    </div>
+  );
+};
+
 function LoginPage() {
   const navigate = useNavigate();  // Initialize navigate function
-
 
   const handleLogin = () => {
     // Add your login validation logic here
@@ -15,64 +21,53 @@ function LoginPage() {
     navigate("/user");
   };
 
-
-    return (
-        <div className="landing-page">
-            <div className="overlap-wrapper">
-              <div className="overlap">
-
-                <div className="overlap-group">
-                  <div className="login"  />
-                  <div className="login-container">
-                    <input 
-                      className="input" 
-                      id="email"
-                      placeholder="Enter Email" 
-                    />
-                    
-                      <input 
-                      type="password" 
-                      className="input"
-                      id="password" 
-                      placeholder="Enter Password" 
-                    />
-                  <div className="illuminate-change">
-                    Illuminate Change
-                    <br />
-                    <br/>
-                    <br/>
-
-                    Empower Communities
-                  </div>
-
-                  <p className="text-wrapper">
-                    Bridging the gap between young changemakers and organizations in
-                    need. From environmental action to education and community
-                    outreach, we make it easy for teens to give back.
-                  </p>
-                  <br/>
-                  <br/>
-                  <br/>
-
-                    <button className="login-button" onClick={handleLogin} >Login</button>
-                  </div>
-                  <div className="text-wrapper-2">Youth In Action</div>
-                  <img
-                    className="photo"
-                    alt="Photo"
-                    src={lightsandcubes}
-                  />
-                  <img
-                    className="photo2"
-                    alt="Photo"
-                    src={loginicons}
-                  />
-                </div>
+  return (
+    <div className="landing-page">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="overlap-group">
+            <Box />
+            <div className="login" />
+            <div className="login-container">
+              <input 
+                className="input" 
+                id="email"
+                placeholder="Enter Email" 
+              />
+              
+              <input 
+                type="password" 
+                className="input"
+                id="password" 
+                placeholder="Enter Password" 
+              />
+              <div className="illuminate-change">
+                Illuminate Change
+                <br />
+                <br/>
+                <br/>
+                Empower Communities
               </div>
+
+              <p className="text-wrapper">
+                Bridging the gap between young changemakers and organizations in
+                need. From environmental action to education and community
+                outreach, we make it easy for teens to give back.
+              </p>
+              <br/>
+              <br/>
+              <br/>
+
+              <button className="login-button" onClick={handleLogin}>Login</button>
             </div>
+            <div className="text-wrapper-2">Youth In Action</div>
+            <img className="photo" alt="Photo" src={lightsandcubes} />
+            <img className="photo2" alt="Photo" src={loginicons} />
           </div>
-    // This is the main div of the LoginPage, it contains all the elements for the login page including the login form, images, and text.
-    )
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
